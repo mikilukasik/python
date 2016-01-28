593956c1d19eb6c6e01fcc4ec495f1c23b90a440
mikilukasik <szarhazi@gmail.com>
2016-01-28 16:15:37 +0000

    .

--- first.py/first.py
+++ first.py/first.py
@@ -15,12 +15,12 @@ userName=raw_input('UserName:') #, please input your name:"
 print "Hello",userName+", what's going on?"
 goingOn=raw_input('Ansver:')
 
-print 'would you like me to put that here a million times?'
+print 'would you like me to put that here a thousand times?'
 wannaSeeThatChar=askYN()
 
 if wannaSeeThatChar:
-    for x in range(1000000):
-        print x,
+    for x in range(1000):
+        print goingOn,
 else:
     print 'Good choice.'
     
@@ -39,6 +39,6 @@ else:
 
 
 
-
+print
 print 'Bye!'
 
